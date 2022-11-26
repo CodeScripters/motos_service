745e4af6a878a17f9447aac0f70eec3aea345462
Cristhian Reinoso <47763412+crisleo94@users.noreply.github.com>
2022-11-26 13:01:19 -0500

    Feat/mot 2/crud MOT-3 MOT-4 ready for testing (#3)
    * base gradle project with spring init, MOT-1 create POST request for Users MOT-9 MOT-10 MOT-11 done
    
    * add swagger plugin for gradle, update README file
    
    * update docker-compose file to persist data in local volume, update models and entities to use the default _id from mongo
    
    * update user controller to validate if a user exists by document before creating, add anotations to userclass for field validation, add hrm to java

--- src/main/java/com/codescripters/motos/User/User.java
+++ src/main/java/com/codescripters/motos/User/User.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.constraints.*;
+import java.math.BigInteger;
 import java.util.Date;
 
 @Data
@@ -15,20 +17,68 @@ import java.util.Date;
 @Document
 public class User {
     @Id
+    @NotNull
+    @NotBlank
     private String id;
+    @NotNull
+    @Size(min = 7, max = 10)
+    private final BigInteger documentNumber;
+    @NotNull
+    @NotBlank
+    @Size(min = 7)
     private final String firstName;
+    @NotNull
+    @NotBlank
+    @Size(min = 7)
     private final String lastName;
+    @NotNull
+    @NotBlank
+    @NotEmpty
     private final Gender gender;
+    @NotNull
+    @NotBlank
+    @NotEmpty
     private final Role role;
-    private final Integer documentNumber;
-    private final Integer licenseNumber;
+    @NotNull
+    @Size(min = 7, max = 10)
+    private final BigInteger licenseNumber;
+    @NotNull
+    @NotBlank
+    @Size(min = 7, max = 30)
     private final String address;
+    @NotNull
+    @NotBlank
+    @Size(min = 7, max = 50)
     private final String city;
+    @NotNull
+    @NotEmpty
+    @NotBlank
+    @Past
+    @Size(min = 3)
     private final Date dateOfBirth;
-    private final Integer phoneNumber;
+    @NotNull
+    @Size(min = 8, max = 10)
+    private final BigInteger phoneNumber;
+    @NotNull
+    @NotBlank
+    @Email
+    @Size(min = 7)
     private final String emailAddress;
     private final Motorcycle motorcycle;
+    @NotNull
+    @NotBlank
+    @Size(min = 4)
     private final String createdBy;
+    @NotNull
+    @NotEmpty
+    @NotBlank
+    @Past
+    @Size(min = 3)
     private final Date createdAt;
+    @NotNull
+    @NotEmpty
+    @NotBlank
+    @Past
+    @Size(min = 3)
     private final Date updatedAt;
 }
--- src/main/java/com/codescripters/motos/User/UserService.java
+++ src/main/java/com/codescripters/motos/User/UserService.java
@@ -1,17 +1,39 @@
 package com.codescripters.motos.User;
 
 import lombok.AllArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
+import java.math.BigInteger;
 import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final MongoTemplate mongoTemplate;
 
     public List<User> getUsers() {
         return userRepository.findAll();
+    }
+
+    public User getUser(BigInteger documentNumber) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("documentNumber").is(documentNumber));
+        List<User> foundUsers = mongoTemplate.find(query, User.class);
+
+        if(foundUsers.size() > 1) {
+            throw new IllegalStateException("Multiple users found with document " + documentNumber.toString());
+        }
+
+        if(foundUsers.isEmpty()) {
+            return null;
+        }
+
+        return foundUsers.get(0);
     }
 
     public User saveUser(User user) {
