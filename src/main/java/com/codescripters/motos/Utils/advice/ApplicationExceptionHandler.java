2c59b2d04585599887b6954fbb57be2ade380cd3
crisleo94 <cristhian.reinoso@live.com>
2022-11-28 04:48:56 -0500

    update head

--- src/main/java/com/codescripters/motos/Utils/advice/ApplicationExceptionHandler.java
+++ src/main/java/com/codescripters/motos/Utils/advice/ApplicationExceptionHandler.java
@@ -2,7 +2,6 @@ package com.codescripters.motos.Utils.advice;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
